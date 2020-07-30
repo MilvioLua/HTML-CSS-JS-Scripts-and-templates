d6e3d3fd7814a7577129f250fbca8436d196da49
Milvio LuA <milviolua@gmail.com>
2020-07-30 21:29:47 +0200

    update

--- 10 minutes exicersices/Starfield in Processing/index.js
+++ 10 minutes exicersices/Starfield in Processing/index.js
@@ -1,18 +1,2 @@
-Star[] stars = new Star[100];
-
-
-void setup() {
-size (400, 400);
-for (int star = 0; i < stars.length; i++) {
-  stars[i] = new Star();
-  }
-}
-
-void draw () {
-  background (0);
-  for (int star = 0; i < stars.length; i++) {
-    stars[i].update();
-    stars[i].show
-    }
-
-}
+var username = prompt();
+alert(username);
